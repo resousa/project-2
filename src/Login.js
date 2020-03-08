23f54c5e5af96de48d6e18cb08c5739f0872a01e
Justin <twoplatinumchainz@gmail.com>
2020-03-08 00:23:02 -0800

    login page WIP

--- src/Login.js
+++ src/Login.js
@@ -1,43 +1,114 @@
 import React, { Component } from 'react';
+import './Login.css';
 
+import { Link } from 'react-router-dom';
 class Login extends Component {
-  // validateForm = () => {
-  //   return email.length > 0 && password.length > 0;
-  // };
+  constructor() {
+    super();
+    this.state = {
+      username: '',
+      password: '',
+      error: ''
+    };
 
-  // submitForm = () => {
-  //   this.props.handleSubmit(this.state);
-  //   this.setState(this.initialState);
-  // };
+    this.handlePassChange = this.handlePassChange.bind(this);
+    this.handleUserChange = this.handleUserChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dismissError = this.dismissError.bind(this);
+  }
+
+  dismissError() {
+      this.setState({ error: '' });
+  }
+
+  handleSubmit(event) {
+      event.preventDefault();
+
+      const userData = {
+          username: this.state.username,
+          password: this.state.password
+      };
+      this.props.loginUser(userData);
+
+    //   if(!this.state.username) {
+    //       return this.setState({ error: 'Username is required' });
+    //   }
+
+    //   if(!this.state.password) {
+    //       return this.setState({ error: 'Password is required' });
+    //   }
+
+    //   return this.setState({ error: '' });
+  }
+
+  handleUserChange(event) {
+      this.setState({
+          username: event.target.value
+      });
+  }
+
+  handlePassChange(event) {
+      this.setState({
+          password: event.target.value
+      })
+  }
 
   render() {
-    // const { email, password } = this.state;
-
     return (
-      <div className='Login'>
-        <div class='container'>
-          <div class='row justify-content-center'>
-            <div class='col-md-6'>
-              <form>
-                <div class='form-group'>
-                  <label for='email'>Email</label>
-                  <input type='email' class='form-control' id='email' />
+        <div className='container login-container'>
+          <div className='row'>
+            <div className='col-md-6 login-form-1'>
+              <h3>Login</h3>
+              <form onSubmit={this.handleSubmit} method='post'>
+                <div className='form-group'>
+                  <input
+                    type='text'
+                    className='form-control'
+                    name='username'
+                    placeholder='Username'
+                    value={this.state.username}
+                    onChange={this.handleUserChange}
+                  />
                 </div>
-                <div class='form-group'>
-                  <label for='password'>Password</label>
-                  <input type='password' class='form-control' id='password' />
+                <div className='form-group'>
+                  <input
+                    type='password'
+                    className='form-control'
+                    name='password'
+                    placeholder='Password'
+                    value={this.state.password}
+                    onChange={this.handlePassChange}
+                  />
                 </div>
-                <input
-                  class='btn btn-primary'
-                  type='button'
-                  value='Login'
-                  onClick={this.submitForm}
-                />
+                <div className='form-group text-center'>
+                  <input
+                    type='submit'
+                    className='btnSubmit'
+                    value='Login Now'
+                  />
+                </div>
+                <div className='form-group'>
+                  Forgot password?{' '}
+                  <a href='http://www.google.com' className='ForgetPwd'>
+                    Click here
+                  </a>
+                </div>
+                <div className='form-group'>
+                  Not a member yet?{' '}
+                  <Link className='ForgetPwd' to='/register'>Sign up</Link>
+                </div>
               </form>
+            </div>
+            <div className='col-md-6'>
+              <img
+                src={require('./assets/images/login-pic.jpg')}
+                width='360px'
+                height='475px'
+                alt='login-pic'
+              />
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
