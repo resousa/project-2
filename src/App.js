4b7f8c6036c8b59f87484bfa67975b46c609dd3d
Justin <twoplatinumchainz@gmail.com>
2020-03-08 08:28:17 -0700

    temp routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+// import { Route } from 'react-router-dom';
+
 import './App.css';
-// import Login from './components/Login';
+// import NavBar from './components/NavBar';
+// import Login from './Login';
+// import Register from './Register';
 import Dashboard from './components/Dashboard';
-
-
 class App extends Component {
   render() {
-      // const { logins } = this.state
-      return (
-              <Dashboard name='Admin'/>
-      )
+    return (
+      <div>
+        {/* <NavBar />
+        <Route exact path='/' component={ Login } />
+        <Route exact path='/register' component={ Register } /> */}
+        <Dashboard />
+      </div>
+    );
   }
 }
 
