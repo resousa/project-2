0d7b8793026715515fd7a658506a7ac7222f0e4b
Justin <twoplatinumchainz@gmail.com>
2020-03-14 14:15:28 -0700

    removed commented out code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
-// import Login from './Login';
-// import Register from './Register';
 import Sidebar from './components/Sidebar';
 import Footer from './components/Footer';
 class App extends Component {
