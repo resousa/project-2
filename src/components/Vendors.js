33db6f435eefd93fe4bda4ee282708834fafd5ee
Justin <twoplatinumchainz@gmail.com>
2020-03-15 09:16:38 -0700

    added functions

--- src/components/Vendors.js
+++ src/components/Vendors.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { Component } from 'react';
+import 'whatwg-fetch';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -8,6 +9,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import './Vendors.css';
+import VendorForm from './VendorForm';
 
 const useStyles = makeStyles({
   table: {
@@ -27,166 +29,149 @@ const rows = [
   createData(`Gingerbread`, 356, 16.0, 49, 3.9)
 ];
 
-export default function Vendors() {
-  const classes = useStyles();
+class Vendors extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      error: null,
+      name: '',
+      contact: '',
+      address: '',
+      inventoryPurchased: '',
+      contractEnd: ''
+    };
+  }
 
-  return (
-    <div className='Vendors'>
-      <div className='container'>
-        <div className='row py-3'>
-          <h1 className='display-4 font-weight-bold'>Vendors</h1>
-        </div>
-        <div className='row'>
-          <TableContainer component={Paper}>
-            <Table className={classes.table} aria-label='simple table'>
-              <TableHead>
-                <TableRow>
-                  <TableCell className='font-weight-bold'>Name</TableCell>
-                  <TableCell className='font-weight-bold' align='right'>
-                    Contact
-                  </TableCell>
-                  <TableCell className='font-weight-bold' align='right'>
-                    Address
-                  </TableCell>
-                  <TableCell className='font-weight-bold' align='right'>
-                    Inventory Purchased
-                  </TableCell>
-                  <TableCell className='font-weight-bold' align='right'>
-                    Contract End
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map(row =>
-                  <TableRow key={row.name}>
-                    <TableCell component='th' scope='row'>
-                      {row.name}
+  componentDidMount() {
+    this.callApi().then(res => this.setState({ response: res.express })).catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/vendors');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  }
+
+  addVendor = e => {
+    e.preventDefault();
+    const {
+      name,
+      contact,
+      address,
+      inventoryPurchased,
+      contractEnd
+    } = this.state;
+    if (!name || !contact || !address || !inventoryPurchased || !contractEnd)
+      return;
+    fetch('/api/vendors', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        contact,
+        address,
+        inventoryPurchased,
+        contractEnd
+      })
+    })
+      .then(res => res.json())
+      .then(res => {
+        if (!res.success)
+          this.setState({ error: res.error.message || res.error });
+        else
+          this.setState({
+            name: '',
+            contact: '',
+            address: '',
+            inventoryPurchased: '',
+            contractEnd: ''
+          });
+      });
+  };
+
+  render() {
+    const classes = useStyles();
+
+    return (
+      <div className='Vendors'>
+        <div className='container'>
+          <div className='row py-3'>
+            <h1 className='display-4 font-weight-bold'>Vendors</h1>
+          </div>
+          <div className='row'>
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label='simple table'>
+                <TableHead>
+                  <TableRow>
+                    <TableCell className='font-weight-bold'>Name</TableCell>
+                    <TableCell className='font-weight-bold' align='right'>
+                      Contact
                     </TableCell>
-                    <TableCell align='right'>{row.contact}</TableCell>
-                    <TableCell align='right'>{row.address}</TableCell>
-                    <TableCell align='right'>
-                      {row.inventoryPurchased}
+                    <TableCell className='font-weight-bold' align='right'>
+                      Address
                     </TableCell>
-                    <TableCell align='right'>{row.contractEnd}</TableCell>
+                    <TableCell className='font-weight-bold' align='right'>
+                      Inventory Purchased
+                    </TableCell>
+                    <TableCell className='font-weight-bold' align='right'>
+                      Contract End
+                    </TableCell>
                   </TableRow>
-                )}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </div>
-        <div className='row py-3'>
-          <div className='col-md-6'>
-            <div className='card'>
-              <div className='card-header font-weight-bold'>Search</div>
-              <div className='card-body'>
-                <form className='form-inline pb-3'>
-                  <input
-                    className='form-control mr-sm-2'
-                    type='search'
-                    placeholder='Search'
-                    aria-label='Search'
-                  ></input>
-                </form>
+                </TableHead>
+                <TableBody>
+                  {rows.map(row => (
+                    <TableRow key={row.name}>
+                      <TableCell component='th' scope='row'>
+                        {row.name}
+                      </TableCell>
+                      <TableCell align='right'>{row.contact}</TableCell>
+                      <TableCell align='right'>{row.address}</TableCell>
+                      <TableCell align='right'>
+                        {row.inventoryPurchased}
+                      </TableCell>
+                      <TableCell align='right'>{row.contractEnd}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+          <div className='row py-3'>
+            <div className='col-md-6'>
+              <div className='card'>
+                <div className='card-header font-weight-bold'>Search</div>
+                <div className='card-body'>
+                  <form className='form-inline pb-3'>
+                    <input
+                      className='form-control mr-sm-2'
+                      type='search'
+                      placeholder='Search'
+                      aria-label='Search'
+                    ></input>
+                  </form>
 
-                <a href='#' className='btn btn-primary'>
-                  Search
-                </a>
+                  <a href='#' className='btn btn-primary'>
+                    Search
+                  </a>
+                </div>
               </div>
             </div>
-          </div>
-          <div className='col-md-6'>
-            <div className='card'>
-              <div className='card-header font-weight-bold'>Add Vendor</div>
-              <div className='card-body'>
-                <div className='input-group mb-3'>
-                  <div className='input-group-prepend'>
-                    <span
-                      className='input-group-text'
-                      id='inputGroup-sizing-default'
-                    >
-                      Name
-                    </span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    aria-label='Default'
-                    aria-describedby='inputGroup-sizing-default'
-                  ></input>
+            <div className='col-md-6'>
+              <div className='card'>
+                <div className='card-header font-weight-bold'>Add Vendor</div>
+                <div className='card-body'>
+                <VendorForm />
                 </div>
-                <div className='input-group mb-3'>
-                  <div className='input-group-prepend'>
-                    <span
-                      className='input-group-text'
-                      id='inputGroup-sizing-default'
-                    >
-                      Contact
-                    </span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    aria-label='Default'
-                    aria-describedby='inputGroup-sizing-default'
-                  ></input>
-                </div>
-                <div className='input-group mb-3'>
-                  <div className='input-group-prepend'>
-                    <span
-                      className='input-group-text'
-                      id='inputGroup-sizing-default'
-                    >
-                      Address
-                    </span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    aria-label='Default'
-                    aria-describedby='inputGroup-sizing-default'
-                  ></input>
-                </div>
-                <div className='input-group mb-3'>
-                  <div className='input-group-prepend'>
-                    <span
-                      className='input-group-text'
-                      id='inputGroup-sizing-default'
-                    >
-                      Inventory Purchased
-                    </span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    aria-label='Default'
-                    aria-describedby='inputGroup-sizing-default'
-                  ></input>
-                </div>
-                <div className='input-group mb-3'>
-                  <div className='input-group-prepend'>
-                    <span
-                      className='input-group-text'
-                      id='inputGroup-sizing-default'
-                    >
-                      Contract End
-                    </span>
-                  </div>
-                  <input
-                    type='text'
-                    className='form-control'
-                    aria-label='Default'
-                    aria-describedby='inputGroup-sizing-default'
-                  ></input>
-                </div>
-
-                <a href='#' className='btn btn-primary'>
-                  Add
-                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default new Vendors();
